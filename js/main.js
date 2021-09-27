fba4ab4c19fe01c0662815883e8316ae2200f262
FirstBroRussia <chipanrussia@gmail.com>
2021-09-27 12:52:20 +0300

    Исправлены недочёты

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-let randomNumber = function (min, max) {
+const randomNumber = function (min, max) {
   if (min < 0 || max < 0 || (max - min) <= 0) {
     return "Введенные параметры не соответствуют условиям: min, max >= 0; max > min";
   }
 let number = Math.round(Math.random() * ((max + 1) - min) + min);
-number > max ? number = Math.floor(number) : number ;
+number > max ? number = max : number ;
 return number;
 }
 
-let stringLength = function (stringValue, defaultLength) {
+const stringLength = function (stringValue, defaultLength) {
   stringValue = String(stringValue);
 if (stringValue.length <= defaultLength) {
   return true;
