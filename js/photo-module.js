5e857e755c36174fc064b935b5288794855b5483
FirstBroRussia <chipanrussia@gmail.com>
2021-10-15 22:37:24 +0300

    Заменен цикл for на метод .forEach

--- js/photo-module.js
+++ js/photo-module.js
@@ -3,18 +3,16 @@ import {createNewPhotos} from './data.js';
 const template = document.querySelector('#picture').content.querySelector('a');
 const picturesFragment = document.createDocumentFragment();
 
-const generatePhotoBlock = function (photoData) {
+const newPhotosData = createNewPhotos;
+
+newPhotosData.forEach( (itemObject) => {
   const photoUser = document.createElement('div');
   const templateClone = template.cloneNode(true);
-  templateClone.querySelector('.picture__img').src = photoData.url;
-  templateClone.querySelector('.picture__comments').textContent = photoData.comments.message;
-  templateClone.querySelector('.picture__likes').textContent = photoData.likes;
+  templateClone.querySelector('.picture__img').src = itemObject.url;
+  templateClone.querySelector('.picture__comments').textContent = itemObject.comments.message;
+  templateClone.querySelector('.picture__likes').textContent = itemObject.likes;
   photoUser.appendChild(templateClone);
   picturesFragment.appendChild(photoUser);
-};
-
-for (let iBasis = 0; iBasis < createNewPhotos.length; iBasis++) {
-  generatePhotoBlock(createNewPhotos[iBasis]);
-}
+});
 
 export {picturesFragment};
