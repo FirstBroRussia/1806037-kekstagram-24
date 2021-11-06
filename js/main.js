2220496e648fc8771b1d552488e4ba9a14ddc25a
Eugene <87717844+FirstBroRussia@users.noreply.github.com>
2021-11-06 19:53:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ import './data.js';
 import './photo-module.js';
 import './render-full-picture.js';
 import './form.js';
-import '/nouislider/nouislider.js';
+import 'nouislider/nouislider.js';
 import './form.js';
